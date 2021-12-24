68e2c3679f97d36614fc7150460ef009439fd84d
a-gehlot <andrew.gehlot@gmail.com>
2021-12-24 15:20:49 -0500

    used ajax to follow/unfollow state change

--- frontend/follow_toggle.js
+++ frontend/follow_toggle.js
@@ -4,6 +4,7 @@ class FollowToggle {
         this.userID = $el.data('user-id');
         this.followState = $el.data('follow-state')
         this.render();
+        this.$el.click(this.handleClick.bind(this))
     }
 
     render() {
@@ -12,6 +13,29 @@ class FollowToggle {
         } else {
             this.$el.text("Unfollow")
         }
+    }
+
+    handleClick(event) {
+        event.preventDefault();
+        $.ajax({
+            type: (this.followState === "unfollowed") ? "POST" : "DELETE",
+            url: "/users/" + this.userID + "/follow",
+            dataType: "json",
+            success: (response) => {
+                this.toggleFollowState();
+                this.render();
+            }
+        });
+    }
+
+    toggleFollowState() {
+        if (this.followState === "unfollowed") {
+            this.$el.data('follow-state', 'followed');
+            this.followState = "followed"
+        } else {
+            this.$el.data('follow-satte', 'unfollowed')
+            this.followState = 'unfollowed'
+        };
     }
 }
 
