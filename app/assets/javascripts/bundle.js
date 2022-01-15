0512bbcb81fdf35ed3ce7861fee7570e184416f6
a-gehlot <andrew.gehlot@gmail.com>
2022-01-15 16:14:19 -0500

    prepending tweets to page via ajax request

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -130,11 +130,19 @@ class TweetCompose {
         let data = this.$el.serializeJSON()
         this.clearInput();
         this.$el.find(":input").prop("disabled", true);
-        APIUtil.createTweet(data);
+        APIUtil.createTweet(data).then((tweet) => this.handleSuccess(tweet));
     }
 
     clearInput() {
         this.$el.find('[name^=tweet]').val('');
+    }
+
+    handleSuccess(tweet) {
+        this.clearInput();
+        console.log(tweet);
+        this.$el.find(":input").prop("disabled", false);
+        let ul = this.$el.data("tweets-ul");
+        $(ul).prepend(`<li>${JSON.stringify(tweet)}</li>`)
     }
 }
 
