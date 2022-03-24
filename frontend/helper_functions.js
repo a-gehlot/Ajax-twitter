62e5e1d2c6080a0a13bd4423f836d0a0bb77f7f3
a-gehlot <andrew.gehlot@gmail.com>
2022-03-24 15:13:47 -0400

    CSS styling for feed

--- frontend/helper_functions.js
+++ frontend/helper_functions.js
@@ -1,8 +1,10 @@
 tweetFormatting = (tweet) => {
-    let $formattedTweet = $('<li></li>')
+    let $formattedTweet = $('<li class="tweet"></li>')
+    let date = new Date(tweet.created_at)
+    date = date.toLocaleDateString('en-us', { weekday: "long", year: "numeric", month: "short" })
     $formattedTweet.append(`${tweet.content}
-        -- <a href="/users/${tweet.user.id}">${tweet.user.username}</a>
-        -- ${tweet.created_at}`)
+        <a href="/users/${tweet.user.id}">${tweet.user.username}</a>
+        ${date}`)
     if (tweet.mentions.length > 0) {
         $formattedTweet.append('<ul></ul>');
         tweet.mentions.forEach((mentioned_user) => {
